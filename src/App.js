f8e2d4017cb8e8ede537349498e82ca8dbbb10aa
Jude OkAGU <juud0010@gmail.com>
2019-07-28 13:05:12 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h3> welcome to my new page now lets start </h3>
+    </div>
+  );
+}
+
+export default App;
